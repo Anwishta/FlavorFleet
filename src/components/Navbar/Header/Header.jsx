99a54d4ce91d4ae5d1ec299c47cb3890c6d04b0e
Anwishta <anwishtaghosh@gmail.com>
2024-09-15 23:35:19 +0530

    food list added

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum cupiditate incidunt accusantium aut soluta repellat natus distinctio recusandae ipsam quisquam fuga quidem vel earum aperiam iste, eius pariatur non consequuntur.</p>
+            <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
